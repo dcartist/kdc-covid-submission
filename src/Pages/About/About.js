3bd872728bbe03bcdfe1ec9d15bfad05c3342cbd
dcartist <pbannerman13@gmail.com>
2022-04-09 11:40:52 -0400

    updating file structure

---
+++ src/Pages/About/About.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Instructions } from "../../Components/Instructions/instructions";
+export default function About() {
+  return (
+   
+    <Instructions />
+  )
+}
