1539a9ea04875c7b360fefdd99b41b8e8423643c
dcartist <pbannerman13@gmail.com>
2022-04-09 12:01:03 -0400

    adding react router dom

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
-import { Instructions } from "./Components/Instructions/instructions";
 import "./styles.css";
 
 export default function App() {
@@ -10,9 +9,7 @@ export default function App() {
 
   return (
     <div className="App">
-      <Instructions />
 
-     
     </div>
   );
 }
