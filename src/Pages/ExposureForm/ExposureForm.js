8ca22ded0f27de0bb8e4ba2c01c2d0fa5927fdf4
dcartist <pbannerman13@gmail.com>
2022-04-09 12:07:34 -0400

    adding the about section

---
+++ src/Pages/ExposureForm/ExposureForm.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ExposureForm() {
+  return (
+    <div>ExposureForm</div>
+  )
+}
--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
+import {Route, Link, Switch, Redirect} from "react-router-dom";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
-
+import About from "./Pages/About/About"
 import "./styles.css";
+
 
 export default function App() {
 
 
   return (
     <div className="App">
-
+      <Switch>
+      <Route path="/about" component={About}/>
+      </Switch>
     </div>
   );
 }
