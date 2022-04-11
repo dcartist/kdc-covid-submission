e15fc22a9bd61df300d44c5d6c22ef6e980385fc
dcartist <pbannerman13@gmail.com>
2022-04-11 00:15:46 -0400

    updating image

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import {Navbar, Nav, Container } from "react-bootstrap"
+import { Link } from 'react-router-dom';
 export default function Navigation() {
   return (
     <>
     
     <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark" className="text-white">
     <Container>
-    <Navbar.Brand href="/" className="logo">ABC Schools</Navbar.Brand>
+    <Navbar.Brand to="/" className="logo" as={Link} >ABC Schools</Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav">
       <Nav className="me-auto">
-        <Nav.Link href="/about">About</Nav.Link>
-        <Nav.Link href="/exposure">Report Covid Case</Nav.Link>
-        <Nav.Link href="/dashboard">Case Dashboard</Nav.Link>
+        <Nav.Link to="/about" as={Link} eventKey={2}>About</Nav.Link>
+        <Nav.Link to="/exposure" as={Link} eventKey={2}>Report Covid Case</Nav.Link>
+        <Nav.Link to="/dashboard" as={Link} eventKey={2}>Case Dashboard</Nav.Link>
        
       </Nav>
     </Navbar.Collapse>
