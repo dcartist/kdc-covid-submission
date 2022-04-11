244d1555f73c3b9ca962757903c12355b7b5c16f
dcartist <pbannerman13@gmail.com>
2022-04-10 23:22:28 -0400

    adding back scss

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./app";
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import './custom.scss';
+import './custom.scss';
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
