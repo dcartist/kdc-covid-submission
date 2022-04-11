6b6d9558023f0ee428b2c78f2aa2463b418ee62b
dcartist <pbannerman13@gmail.com>
2022-04-10 23:04:15 -0400

    updating index

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./app";
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import './custom.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import './custom.scss';
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
